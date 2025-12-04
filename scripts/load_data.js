6097c53979a4c63cc149514ab0aa05494c7b098c
abhilash-techversant <abhilash.antony@techversantinfotech.com>
2025-12-04 14:45:53 +0530

    update excel data

--- scripts/load_data.js
+++ scripts/load_data.js
@@ -38,13 +38,13 @@ function loadData() {
         console.log('Sub Topics Columns:', Object.keys(subTopicsRaw[0] || {}));
 
         // Link Overrides (Hardcoded per user request)
-        const LINK_OVERRIDES = {
-            "Basics of GenAI": "https://docs.google.com/document/d/1eFNvfXvnvg8zaVc7UnCYuoOeiRnccnxJzxsqXaaEkXQ/edit?usp=sharing",
-            "Postgres, Pgadmin4": "https://docs.google.com/document/d/1pD3BrnP6jJ-C0SQjvdDH9o_z3-dgqzY9emVP1GsYhSs/edit?usp=sharing",
-            "Docker": "https://docs.google.com/document/d/1HnhcFHjqWiHk4LkF_sZGLQpLyTqckk6zZYMqmtxf_dg/edit?usp=sharing",
-            "FastAPI": "https://docs.google.com/document/d/1O89BV4dYKDRRb-bW7dL0vIlIBumCMC38KXVjpw7uQcw/edit?usp=sharing",
-            "Git, Github": "https://docs.google.com/document/d/13SlpZbMJ52VxuuaGhp9Z7lY60it2GHCgS44sJsr6iWQ/edit?usp=sharing"
-        };
+        // const LINK_OVERRIDES = {
+        //     "Basics of GenAI": "https://docs.google.com/document/d/1eFNvfXvnvg8zaVc7UnCYuoOeiRnccnxJzxsqXaaEkXQ/edit?usp=sharing",
+        //     "Postgres, Pgadmin4": "https://docs.google.com/document/d/1pD3BrnP6jJ-C0SQjvdDH9o_z3-dgqzY9emVP1GsYhSs/edit?usp=sharing",
+        //     "Docker": "https://docs.google.com/document/d/1HnhcFHjqWiHk4LkF_sZGLQpLyTqckk6zZYMqmtxf_dg/edit?usp=sharing",
+        //     "FastAPI": "https://docs.google.com/document/d/1O89BV4dYKDRRb-bW7dL0vIlIBumCMC38KXVjpw7uQcw/edit?usp=sharing",
+        //     "Git, Github": "https://docs.google.com/document/d/13SlpZbMJ52VxuuaGhp9Z7lY60it2GHCgS44sJsr6iWQ/edit?usp=sharing"
+        // };
 
         // Process Data
         const topics = mainTopicsRaw.map(row => {
